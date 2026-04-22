c6f1fa0da38b849f75db5654dc71971d57e4065b
mkandreum <mkandreum@gmail.com>
2026-04-22 12:32:27 +0200

    feat: containerize frontend with Docker and configure API proxying in docker-compose

--- salon-dashboard-frontend/next.config.js
+++ salon-dashboard-frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://backend:5000/:path*', // Redirige automáticamente las llamadas de /api al backend
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
